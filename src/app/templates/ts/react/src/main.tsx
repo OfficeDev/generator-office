ee80294d22a1d40c3bdd78a9af76833fd23c3b09
Bhargav Krishna <bhargav.krishna@outlook.com>
2017-03-28 17:06:31 -0700

    Adding progress spinner before office initialize

--- src/app/templates/ts/react/src/main.tsx
+++ src/app/templates/ts/react/src/main.tsx
@@ -1,15 +1,22 @@
 import * as React from 'react';
 import { render } from 'react-dom';
 import { App } from './components/app';
+import { Progress } from './components/progress';
 import './assets/styles/global.scss';
 
-function main() {
-    render(
-        <App title='<%= projectDisplayName %>' />,
-        document.querySelector('#container')
-    );
-}
+(() => {
+    const title = '<%= projectDisplayName %>';
+    const container = document.querySelector('#container');
 
-Office.initialize = () => {
-    main();
-};
+    /* Render application after Office initializes */
+    Office.initialize = () => {
+        render(
+            <App title={title} />,
+            container
+        );
+    };
+
+    /* Initial render showing a progress bar */
+    render(<Progress title={title} logo='assets/logo-filled.png' message='Loading' />, container);
+})();
+
