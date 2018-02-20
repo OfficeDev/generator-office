10c3b294d8e1b970db0833cfb6dcef4c8f8bfc46
Colby Sieber <colbys@microsoft.com>
2018-02-20 11:24:09 -0800

    Use generated certs rather than internal webpack-dev-server ones

--- src/app/templates/ts/react/config/webpack.dev.js
+++ src/app/templates/ts/react/config/webpack.dev.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const path = require('path');
 const webpack = require('webpack');
 const webpackMerge = require('webpack-merge');
@@ -9,7 +10,11 @@ module.exports = webpackMerge(commonConfig, {
         publicPath: '/',
         contentBase: path.resolve('dist'),
         hot: true,
-        https: true,
+        https: {
+            key: fs.readFileSync('./certs/server.key'),
+            cert: fs.readFileSync('./certs/server.crt'),
+            cacert: fs.readFileSync('./certs/ca.crt')
+        },
         compress: true,
         overlay: {
             warnings: false,
