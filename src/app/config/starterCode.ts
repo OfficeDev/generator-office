b73a15c70ae3c5d939fde48c28e3911e75e06355
Colby Sieber <colbys@microsoft.com>
2018-03-19 11:42:49 -0700

    Added Excel and Word specific starter code snippets

--- src/app/config/starterCode.ts
+++ src/app/config/starterCode.ts
@@ -1,13 +1,63 @@
+/**
+ * Generates an empty RichAPI-based starter template for the provided host
+ * 
+ * @param host The host name
+ */
 const base = (host: string) =>
-        `return ${host}.run(async context => {
+        `return ${host}.run(context => {
             /**
              * Insert your ${host} code here
              */
             return context.sync();
         });`;
 
+/**
+ * Generates a starter code snippet for the provided host.
+ * This snippet may be host-specific if the provide host is recognized,
+ * otherwise it will be a generic base snippet.
+ * 
+ * @param host The host name
+ */
 export default (host: string) => {
     switch (host) {
+        case 'Excel':
+            return (
+        `return ${host}.run(context => {
+            /**
+             * Insert your ${host} code here
+             */
+            const range = context.workbook.getSelectedRange();
+            
+            // Read the range address
+            range.load('address');
+
+            // Update the fill color
+            range.format.fill.color = 'yellow';
+
+            return context.sync().then(() => 
+                console.log(\`The range address was \${range.address}.\`);
+            );
+        });`
+            );
+        case 'Word':
+            return (
+        `return ${host}.run(context => {
+            /**
+             * Insert your ${host} code here
+             */
+            const range = context.document.getSelection();
+            
+            // Read the range text
+            range.load('text');
+
+            // Update font color
+            range.font.color = 'red';
+
+            return context.sync().then(() => 
+                console.log(\`The selected text was \${range.text}.\`);
+            );
+        });`
+            );
         case 'Outlook':
             // Outlook doesn't use RichAPI and has an empty sample
             return (
