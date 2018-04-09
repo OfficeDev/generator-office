08ef363e742365644cf9a74e641ee64d4fc5a9bf
Courtney Owen <cowen@microsoft.com>
2018-04-09 15:16:04 -0700

    remove change to src/app/templates/js/jquery/app.js

--- src/app/templates/js/jquery/app.js
+++ src/app/templates/js/jquery/app.js
@@ -20,18 +20,7 @@
     /**
      * Insert your <%= host %> code here
      */
-    <% } else if (host === 'PowerPoint') { %><%# PowerPoint doesn't use RichAPI %>
-    /**
-     * Insert your <%= host %> code here
-     */
-    Office.context.document.setSelectedDataAsync('Hello World!', {
-        coercionType: Office.CoercionType.Text
-    }, result => {
-        if (result.status === Office.AsyncResultStatus.Failed) {
-            console.error(result.error.message);
-        }
-    });
-<% } else { %>
+    <% } else { %>
     return <%= host %>.run(function (context) {
       /**
        * Insert your <%= host %> code here
