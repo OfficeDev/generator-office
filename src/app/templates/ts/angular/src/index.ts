f4fb3dcbf0e86f8c7066b1d86e047806abae3ede
Colby Sieber <colbys@microsoft.com>
2018-04-11 16:20:46 -0700

    Added bootstrap to angular templates

--- src/app/templates/ts/angular/src/index.ts
+++ src/app/templates/ts/angular/src/index.ts
@@ -2,5 +2,9 @@ import 'zone.js'; // Required for Angular
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import AppModule from './app/app.module';
 
-// Bootstrap the app
-platformBrowserDynamic().bootstrapModule(AppModule).catch(error => console.error(error));
+Office.initialize = reason => {
+    document.getElementById('sideload-msg').style.display = 'none';
+
+    // Bootstrap the app
+    platformBrowserDynamic().bootstrapModule(AppModule).catch(error => console.error(error));
+};
