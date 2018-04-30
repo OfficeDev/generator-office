874823b798449f2809584e100f653e28fcde4a58
Courtney Owen <cowen@microsoft.com>
2018-04-30 14:10:29 -0700

    Make minor changes to Yo Office to plug in Excel Custom Functions template; Only show Excel Custom Functions framework if selected host type is Excel; capitalize template dirs and manifest names so we can don't have to capitalize via code

---
+++ src/app/templates/Excel Custom Functions (Preview)/startserver.js
@@ -0,0 +1,5 @@
+var httpserver = require('http-server');
+server = httpserver.createServer(function(request,response){
+    }).listen(8080, "localhost");
+    console.log('Server running at http://localhost:8080/');
+
