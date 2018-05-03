773aca8a354ec40780fb8dc434f875901b471a0d
Courtney Owen <cowen@microsoft.com>
2018-05-03 16:05:30 -0700

    add latest version of customfunction files

--- src/app/templates/Excel Custom Functions (Preview)/customfunctions.js
+++ src/app/templates/Excel Custom Functions (Preview)/customfunctions.js
@@ -1,26 +1,44 @@
-﻿function ADD42(num) {
-	return num + 42;
+﻿function ADD42(num1, num2) {
+	return num1 + num2 + 42;
 }
 
-function ADD42ASYNC(num) {
+// The following function is an example of an asynchronous function:
+function ADD42ASYNC(num1, num2) {
 	// waits 1 second before returning the result
 	return new OfficeExtension.Promise(function(resolve) {
 		setTimeout(function() {
-			resolve(num + 42);
+			resolve(num1 + num2 + 42);
 		}, 1000);
 	});
 }
 
-function ISEVEN(num) {
-	return num % 2 == 0;
+// The following function is an example that returns a boolean value:
+function ISPRIME(num) {
+	if(num <= 1 || !Number.isInteger(num)) return false; // validates input
+	// checks natural numbers below the square root (not optimal)
+	for(var factor = Math.floor(Math.sqrt(num)); factor > 1; factor--){
+    		if(num % factor == 0) return false;
+  	}
+  	return true;
 }
 
+// The following function is an example that's computation-intensive for high inputs:
+function NTHPRIME(n){
+  	var primeCount = 0;
+  	for(var num = 2; primeCount < n; num++){
+    		if(ISPRIME(num)) primeCount++;
+  	}
+  	return num - 1;  
+}
+
+// The following function is an example that returns a string:
 function GETDAY() {
 	var d = new Date();
 	var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 	return days[d.getDay()];
 }
 
+// The following function is an example that streams values:
 function INCREMENTVALUE(increment, caller){    
 	var result = 0;
 	var myInterval = setInterval(function(){
@@ -32,6 +50,7 @@ function INCREMENTVALUE(increment, caller){
 	}
 }
 
+// The following function is an example that uses a range of cells:
 function SECONDHIGHEST(range){
 	var highest = range[0][0], secondHighest = range[0][0];
 	for(var i = 0; i < range.length;i++){
