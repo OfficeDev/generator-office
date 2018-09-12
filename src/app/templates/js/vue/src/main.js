53ccc025b58d4ca1c45746fa24040be51c5e4365
Riwut Libinuko <cakriwut@hotmail.com>
2018-09-13 07:04:44 +0800

    Ignore eslint emptylines

--- src/app/templates/js/vue/src/main.js
+++ src/app/templates/js/vue/src/main.js
@@ -1,10 +1,11 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+/* eslint-disable no-multiple-empty-lines */
 import Vue from 'vue'
 import App from './App'
 
 <%- imports %>
-
+/* eslint-enable no-multiple-empty-lines */
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
