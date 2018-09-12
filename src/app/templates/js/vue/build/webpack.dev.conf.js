cd845be7c9a990bffc78ab4dbe5882a0cc6e1788
Riwut Libinuko <cakriwut@hotmail.com>
2018-09-13 07:04:58 +0800

    Update url

--- src/app/templates/js/vue/build/webpack.dev.conf.js
+++ src/app/templates/js/vue/build/webpack.dev.conf.js
@@ -82,7 +82,7 @@ module.exports = new Promise((resolve, reject) => {
       // Add FriendlyErrorsPlugin
       devWebpackConfig.plugins.push(new FriendlyErrorsPlugin({
         compilationSuccessInfo: {
-          messages: [`Your application is running here: http://${devWebpackConfig.devServer.host}:${port}`],
+          messages: [`Your application is running here: https://${devWebpackConfig.devServer.host}:${port}`],
         },
         onErrors: config.dev.notifyOnErrors
         ? utils.createNotifierCallback()
