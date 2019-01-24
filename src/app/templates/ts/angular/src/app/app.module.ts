3aed7fb84305145c228ed3ab39863bd4942fc8a1
Sam Severance <sam.severance@merrillcorp.com>
2019-01-24 15:36:38 -0500

    Remove Unused Import Statement

--- src/app/templates/ts/angular/src/app/app.module.ts
+++ src/app/templates/ts/angular/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import AppComponent from './app.component';
 
