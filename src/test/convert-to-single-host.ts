22bf9e3295ad3087eee5eadcac591dcac13202a0
Julian Florez <t-juflor@microsoft.com>
2019-06-27 13:21:49 -0700

    Removed unneeded prerealse references from convert-to-single-host.ts, as it's no longer needed.

--- src/test/convert-to-single-host.ts
+++ src/test/convert-to-single-host.ts
@@ -12,7 +12,6 @@ const hosts = ["excel", "onenote", "outlook", "powerpoint", "project", "word"];
 const manifestFile = "manifest.xml";
 const packageJsonFile = "package.json";
 // For now, all tests will be run with the prerelease option until convert-to-single-host is merged to the yo-office branches
-const prerelease = process.argv.indexOf('--prerelease') > -1 || true;
 const readFileAsync = promisify(fs.readFile);
 const unexpectedManifestFiles = [
     'manifest.excel.xml',
@@ -49,7 +48,7 @@ describe('Office-Add-Taskpane-Ts projects', () => {
 
     describe('Office-Add-Taskpane project', () => {
         before((done) => {
-            helpers.run(path.join(__dirname, '../app')).withOptions(prerelease ? { 'prerelease': true } : {}).withPrompts(answers).on('end', done);
+            helpers.run(path.join(__dirname, '../app')).withPrompts(answers).on('end', done);
         });
 
         it('creates expected files', (done) => {
@@ -111,7 +110,7 @@ describe('Office-Add-Taskpane-Angular-Js project', () => {
 
     describe('Office-Add-Taskpane project', () => {
         before((done) => {
-            helpers.run(path.join(__dirname, '../app')).withOptions(prerelease ? { 'prerelease': true } : {}).withPrompts(answers).on('end', done);
+            helpers.run(path.join(__dirname, '../app')).withPrompts(answers).on('end', done);
         });
 
         it('creates expected files', (done) => {
@@ -172,7 +171,7 @@ describe('Office-Add-Taskpane-React-Ts project', () => {
 
     describe('Office-Add-Taskpane project', () => {
         before((done) => {
-            helpers.run(path.join(__dirname, '../app')).withOptions(prerelease ? { 'prerelease': true } : {}).withPrompts(answers).on('end', done);
+            helpers.run(path.join(__dirname, '../app')).withPrompts(answers).on('end', done);
         });
 
         it('creates expected files', (done) => {
