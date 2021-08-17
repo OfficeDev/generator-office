918f8c55f6f7e9ac682f554861d77e2c6ca3dc98
Darren Miller <darrmill@microsoft.com>
2021-08-17 11:00:56 -0700

    Flatten output conditional

--- src/app/index.ts
+++ src/app/index.ts
@@ -383,21 +383,19 @@ module.exports = class extends yo {
     } else if (this.project.isManifestOnly) {
       this.log(`      2. Open the project in VS Code:\n`);
       this.log(`         ${chalk.bold('code .')}\n`);
+    } else if (this.project.host === "Excel" || this.project.host === "Word" || this.project.host === "Powerpoint" || this.project.host === "Outlook") {
+      this.log(`      2. Start the local web server and sideload the add-in:\n`);
+      this.log(`         ${chalk.bold('npm start')}\n`);
+      this.log(`      3. Open the project in VS Code:\n`);
+      this.log(`         ${chalk.bold('code .')}\n`);
     } else {
-      if (this.project.host === "Excel" || this.project.host === "Word" || this.project.host === "Powerpoint" || this.project.host === "Outlook") {
-        this.log(`      2. Start the local web server and sideload the add-in:\n`);
-        this.log(`         ${chalk.bold('npm start')}\n`);
-        this.log(`      3. Open the project in VS Code:\n`);
-        this.log(`         ${chalk.bold('code .')}\n`);
-      } else {
-        this.log(`      2. Start the local web server:\n`);
-        this.log(`         ${chalk.bold('npm run dev-server')}\n`);
-        this.log(`      3. Sideload the the add-in:\n`);
-        this.log(`         ${chalk.bold('Follow these instructions:')}`);
-        this.log(`         ${defaults.networkShareSideloadingSteps}\n`);
-        this.log(`      4. Open the project in VS Code:\n`);
-        this.log(`         ${chalk.bold('code .')}\n`);
-      }
+      this.log(`      2. Start the local web server:\n`);
+      this.log(`         ${chalk.bold('npm run dev-server')}\n`);
+      this.log(`      3. Sideload the the add-in:\n`);
+      this.log(`         ${chalk.bold('Follow these instructions:')}`);
+      this.log(`         ${defaults.networkShareSideloadingSteps}\n`);
+      this.log(`      4. Open the project in VS Code:\n`);
+      this.log(`         ${chalk.bold('code .')}\n`);
     }
     this.log(`         For more information, visit http://code.visualstudio.com.\n`);
     this.log(`      Please visit https://docs.microsoft.com/office/dev/add-ins for more information about Office Add-ins.\n`);
